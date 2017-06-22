e6781ab44d6295676af2312d14f322a5a1fa1fcd
rui zhang <rui.zhang@tendcloud.com>
2017-06-22 18:38:43 +0800

    调整数据库操作

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, current_app, request
+from flask import Blueprint, render_template, current_app, request, session
 
 from tools.database import Database
 
@@ -10,8 +10,10 @@ def login():
     username = request.form['username']
     password = request.form['password']
     db = Database(current_app.config['DB_CONFIG'])
-    result = db.query("SELECT * FROM user WHERE username=%s AND password =%s", username, password)
-    if result is not None:
+    curr_user = db.query("SELECT * FROM user WHERE username=%s AND password =%s", username, password)
+    if curr_user is not None:
+        session['is_login'] = True
+        session['user'] = curr_user
         return 'login'
     else:
         error = 'Invalid username or password!'
--- tools/database.py
+++ tools/database.py
@@ -9,20 +9,41 @@ class Database:
     def __connect(self):
         return pymysql.connect(**self.config)
 
-    def query(self, sql_query, *args):
-        conn = self.__connect()
-        with conn.cursor() as cursor:
-            cursor.execute(sql_query, args)
-            result = cursor.fetchone()
-        conn.commit()
-        conn.close()
-        return result
+    def find_one(self, sql_query, *args):
+        return self.__find(sql_query, True, args)
 
-    def insert(self, sql_delete):
-        pass
+    def find_all(self, sql_query, *args):
+        return self.__find(sql_query, False, args)
 
-    def update(self, sql_update):
-        pass
+    def insert(self, sql_insert, *args):
+        return self.__run(sql_insert, args)
+
+    def update(self, sql_update, *args):
+        return self.__run(sql_update, args)
 
     def delete(self, table, primary_key):
-        pass
+        sql = 'delete from ' + table + ' where id=%s'
+        return self.__run(sql, primary_key)
+
+    def __find(self, sql_query, is_one, *args):
+        try:
+            conn = self.__connect()
+            with conn.cursor() as cursor:
+                cursor.execute(sql_query, args)
+                if is_one:
+                    result = cursor.fetchone()
+                else:
+                    result = cursor.fetchall()
+            return result
+        finally:
+            conn.close()
+
+    def __run(self, sql, *args):
+        try:
+            conn = self.__connect()
+            with conn.cursor() as cursor:
+                result = cursor.execute(sql, args)
+            conn.commit()
+            return result
+        finally:
+            conn.close()
